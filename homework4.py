c3836bf473a83b11b2ad3ae3faf67ebb7be60349
igor0073 <166950497+igor0073@users.noreply.github.com>
2024-05-03 19:33:12 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,13 @@
+
+tuple_ = 1, 2, 'fuud', False
+print(tuple_)
+tuple_ = (['fuud'], 5, 10)
+print(tuple_)
+tuple_[0][0]= 5
+print(tuple_)
+list = [1, 5, 'student', 'doktor', True]
+print(list)
+list [0] = 'game'
+print(list)
+list [2] = 'driver'
+print(list)
